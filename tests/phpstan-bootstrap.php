247d261c794ca71abdccc1798ab6d61e25f5f284
kstmonowar <kstmonowar@gmail.com>
2026-07-09 09:35:22 +0600

    Add PHPStan static analysis (level 5, clean)

---
+++ tests/phpstan-bootstrap.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * PHPStan bootstrap: constants the plugin defines at runtime.
+ *
+ * @package General_Slider
+ */
+
+// phpcs:disable WordPress.NamingConventions.PrefixAllGlobals -- these mirror runtime constants defined elsewhere.
+
+define( 'GENERAL_SLIDER_FILE', __DIR__ . '/../general-slider.php' );
+define( 'GENERAL_SLIDER_DIR', __DIR__ . '/../' );
+define( 'GENERAL_SLIDER_URL', 'https://example.com/wp-content/plugins/general-slider/' );
+define( 'GENERAL_SLIDER_BASENAME', 'general-slider/general-slider.php' );
+define( 'GENERAL_SLIDER_VERSION', '0.0.0' );
+define( 'GS_DEMO_LIBRARY_URL', 'https://devmonowar.github.io/wp-plugin-demo-library/general-slider/demo-library.json' );
+define( 'WP_UNINSTALL_PLUGIN', 'general-slider/general-slider.php' );
